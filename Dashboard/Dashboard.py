54b449c1a78df4d0c896cb15e4ffbe6dfe41ce8e
Leonid Sh <shmidt.leonidt@gmail.com>
2024-03-29 14:44:04 +0300

    start now

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -6,3 +6,13 @@ import dash_auth
 from Dashboard.Calculations import #choose_hospital_ASC
 from Dashboard.Filters import #sidebar
 from Dashboard.Content import #content
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal')
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
