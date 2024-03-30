0270b40ef60829464e825649d9041e51b8c221e2
Leonid Sh <shmidt.leonidt@gmail.com>
2024-03-30 20:03:41 +0300

    add blocks

--- Dashboard/Body.py
+++ Dashboard/Body.py
@@ -1,56 +1,41 @@
 from dash import Dash, html, dcc, dash_table, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import dash_html_components as html
 
 from Parsing_data import df 
-
+from Filters import choosing_a_hospital, choosing_a_year, choosing_the_month, table
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.Div(children='Выберете больницу'),
-    html.Div([ 
-        dcc.Checklist(            
-            ['ГОБУЗ "Боровичская ЦРБ"','ГОБУЗ "Старорусская ЦРБ"', 'ГОБУЗ "НОКБ"'],
-            ['ГОБУЗ "Боровичская ЦРБ"','ГОБУЗ "Старорусская ЦРБ"', 'ГОБУЗ "НОКБ"'], id='checkbox'), 
-        html.Div(children='---'),
-        dcc.Checklist(            
-            ['Г','ГО', 'ГОБ'],
-            ['Г','ГО', 'ГОБ'], id='heckbox'), 
-        html.Div(children='Табличные данные'),
-        dcc.RangeSlider(
-                        min=1, max=12, 
-                        step=1,
-                        value=[1,12],
-                        tooltip={"placement": "bottom", "always_visible": False},
-                        vertical = True,
-                        verticalHeight = 300,
-                        id='my-slider'
-                        ),
-        html.Div(id='slider-output-container'),
-        html.Div(id='datatable-container')
-        ])
+app.layout =  html.Div( children= [
+                        choosing_a_hospital,
+                        choosing_a_year, 
+                        choosing_the_month, 
+                        table
 ])
 
 
 @callback(
     Output('datatable-container', 'children'),
-    Input('checkbox', 'value'),
-    Input('my-slider', 'value')
+    Input('choose_a_hospital', 'value'),
+    Input('monthly_slider', 'value'),
+    Input('choose_a_year', 'value')
 )
-def update_datatable(selected_hospital, month):
-    if not selected_hospital:
+def update_datatable(selected_hospital, month, year):
+    if not selected_hospital or not year:
         return []
-    new_df = df[df['Hospital'].isin(selected_hospital) & df['Month'].isin(range(month[0], month [1]))]
+    t_year=[int (x) for x in year]
+    new_df = df[(df['Hospital'].isin(selected_hospital)) & (df['Month'].isin(range(month[0], month [1]+1))) & (df['Date'].dt.year.isin (t_year))]
     return dash_table.DataTable(
+        style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },
         data=new_df.to_dict('records'), 
-        page_size=50
+        page_size=30
 )
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-
-
-    new_df = df[df['Hospital'].isin('ГОБУЗ "Боровичская ЦРБ"')] & df[df['Month'].isin(8)]
--- Dashboard/Filters.py
+++ Dashboard/Filters.py
@@ -1,8 +1,83 @@
-#import pandas as pd
-#from dash import Dash, dcc, html, dash_table, callback, Output, Input
-#import dash_bootstrap_components as dbc
+import pandas as pd
+from dash import Dash, dcc, html, dash_table, callback, Output, Input
+import calendar
+import locale
+from Parsing_data import df
 
-#from Dashboard.Parsing_data import df
-#from Dashboard.Parsing_data import df
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
 
-from Parsing_data import df 
+
+choosing_a_hospital = html.Div(children=[
+                        html.H3(children='Выберете больницу'),
+                        html.P([
+                            dcc.Checklist(            
+                                ['ГОБУЗ "Боровичская ЦРБ"','ГОБУЗ "Старорусская ЦРБ"', 'ГОБУЗ "НОКБ"'],
+                                ['ГОБУЗ "Боровичская ЦРБ"','ГОБУЗ "Старорусская ЦРБ"', 'ГОБУЗ "НОКБ"'], 
+                                id='choose_a_hospital'
+                                ),
+                        html.Hr()
+                                ])
+                            ],
+                        style={'display': 'block', 
+                        #'backgroundColor': '#00FFFF',   #HEX-коды цветов
+                        'fontSize': '15px',
+                        'margin-left': '10px', 
+                        'width': '200px', 
+                        'height': '105px'
+                        })
+#
+                            
+choosing_a_year = html.Div(children=[
+                    html.H3(children='Выберете год'),
+                        html.P([
+                        dcc.Checklist(            
+                            ['2022','2023'],
+                            ['2022','2023'], 
+                            id='choose_a_year'
+                                    ),
+                           html.Hr()
+                           ])
+                        ],
+                        style={'display': 'block', 
+                        #'backgroundColor': '#00FFFF',   #HEX-коды цветов
+                        'fontSize': '15px',
+                        'margin-left': '10px', 
+                        'width': '200px', 
+                        'height': '90px'
+                        })
+
+choosing_the_month= html.Div(children=[
+                        html.H3(children='Выберете месяц'),
+                        html.Div(children=[
+                            dcc.RangeSlider(
+                                min=1, max=12, 
+                                step=1,
+                                value=[1,12],
+                                marks={i: calendar.month_name[i] for i in range(1, 13)},
+                                tooltip={"placement": "bottom", "always_visible": False},
+                                vertical = True,
+                                verticalHeight = 300,
+                                id='monthly_slider'
+                                )], 
+                            style={
+                                'display': 'block', 
+                                #'backgroundColor': '#00FFFF',   #HEX-коды цветов
+                                'fontSize': '14px',
+                                'margin-left': '35px', 
+                                'width': '20px', 
+                                'height': '3px'}
+                                )
+                        ],   
+                        style={'display': 'block', 
+                        #'backgroundColor': '#00FFFF',   #HEX-коды цветов
+                        'fontSize': '15px',
+                        'margin-left': '10px', 
+                        'width': '200px', 
+                        'height': '360px'
+                        }
+                    )
+
+table=html.Div([
+    html.H1('Табличные данные'),
+    html.Div(id='datatable-container')
+            ])
